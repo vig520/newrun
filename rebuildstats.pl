2ef6ada477d7791516fdd25dc4d6b1fa9bf56be3
vig520 <simecek.tomas@gmail.com>
2016-02-11 09:10:47 +0100

    Pridano prepocitani statistik

--- rebuildstats.pl
+++ rebuildstats.pl
@@ -26,10 +26,10 @@ while (@DATA =  $QUERY->fetchrow_array) {
 	push @SQL, 'BehuRoku.'.$DATA[0],$DATA[1];
 }
 
-$QUERY = $dbh->prepare("select StartCountry, count(StartCountry) AS Kolikrat from tblRuns where StartCountry <> 'NULL' group by StartCountry;");
+$QUERY = $dbh->prepare("select StartCountry, tblCountries.Primary, count(StartCountry) AS Kolikrat from tblRuns, tblCountries where StartCountry <> 'NULL' and StartCountry = tblCountries.Country group by StartCountry;");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 3!";
 while (@DATA =  $QUERY->fetchrow_array) {
-	push @SQL, 'Zeme.'.$DATA[0],$DATA[1];
+	push @SQL, 'Zeme.'.$DATA[0].'.'.$DATA[1],$DATA[2];
 }
 
 $QUERY = $dbh->prepare("select count(Home) as Home from tblRuns where Home = 1;");
@@ -38,14 +38,14 @@ while (@DATA =  $QUERY->fetchrow_array) {
         push @SQL, 'Home',$DATA[0];
 }
 
-$QUERY = $dbh->prepare("select ROUND(SUM(Distance/1000),2) AS ThisMonthDistance, Monthname(CURDATE()) AS Mesic from tblRuns where Year(convert_tz(Date, 'UTC','CET')) = Year(CURDATE()) and Month(convert_tz(Date, 'UTC','CET')) = Month(CURDATE());");
+$QUERY = $dbh->prepare("select ROUND(SUM(Distance/1000),2) AS ThisMonthDistance, Month(CURDATE()) AS Mesic from tblRuns where Year(convert_tz(Date, 'UTC','CET')) = Year(CURDATE()) and Month(convert_tz(Date, 'UTC','CET')) = Month(CURDATE());");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 5!";
 while (@DATA =  $QUERY->fetchrow_array) {
         push @SQL, 'ThisMonthMileage',$DATA[0];
 	push @SQL, 'ThisMonthName',$DATA[1];
 }
 
-$QUERY = $dbh->prepare("select ROUND(SUM(Distance/1000),2) AS LastMonthDistance, MONTHNAME(DATE_ADD(CURDATE(), INTERVAL -1 MONTH)) AS MinulyMesic from tblRuns where Year(convert_tz(Date, 'UTC','CET')) = Year(DATE_ADD(CURDATE(), INTERVAL -1 MONTH)) and MONTHNAME(convert_tz(Date, 'UTC','CET')) = MONTHNAME(DATE_ADD(CURDATE(), INTERVAL -1 MONTH));");
+$QUERY = $dbh->prepare("select ROUND(SUM(Distance/1000),2) AS LastMonthDistance, MONTH(DATE_ADD(CURDATE(), INTERVAL -1 MONTH)) AS MinulyMesic from tblRuns where Year(convert_tz(Date, 'UTC','CET')) = Year(DATE_ADD(CURDATE(), INTERVAL -1 MONTH)) and MONTHNAME(convert_tz(Date, 'UTC','CET')) = MONTHNAME(DATE_ADD(CURDATE(), INTERVAL -1 MONTH));");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 6!";
 while (@DATA =  $QUERY->fetchrow_array) {
         push @SQL, 'LastMonthMileage',$DATA[0];
@@ -55,14 +55,14 @@ $QUERY = $dbh->prepare("SET \@nextDate = CURRENT_DATE;");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 7!";
 $QUERY = $dbh->prepare("SET \@RowNum = 1;");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 7!";
-$QUERY = $dbh->prepare("SELECT  \@RowNum := IF(\@NextDate = Date(tblRuns.Date), \@RowNum + 1, 1) AS RowNumber, DATE_FORMAT((tblRuns.Date), '%d. %m. %Y') AS Datum, tblRuns.`Primary`, DATE_ADD(Date(tblRuns.Date), INTERVAL (\@RowNum*(-1))+1 DAY) AS StartDate, \@NextDate := DATE_ADD(Date(tblRuns.Date), INTERVAL 1 DAY) AS NextDate FROM tblRuns ORDER BY RowNumber DESC LIMIT 1;");
+$QUERY = $dbh->prepare("SELECT  \@RowNum := IF(\@NextDate = Date(tblRuns.Date), \@RowNum + 1, 1) AS RowNumber, DATE_FORMAT((tblRuns.Date), '%d. %m. %Y') AS Datum, tblRuns.`Primary`, DATE_FORMAT(DATE_ADD(Date(tblRuns.Date), INTERVAL (\@RowNum*(-1))+1 DAY), '%d. %m. %Y') AS StartDate, \@NextDate := DATE_ADD(Date(tblRuns.Date), INTERVAL 1 DAY) AS NextDate, DATE_ADD(Date(tblRuns.Date), INTERVAL (\@RowNum*(-1))+1 DAY) AS StartDate2 FROM tblRuns ORDER BY RowNumber DESC LIMIT 1;");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 7!";
 while (@DATA =  $QUERY->fetchrow_array) {
         push @SQL, 'DniPoSobe',$DATA[0];
         push @SQL, 'SerieKonci',$DATA[1];
 	push @SQL, 'PosledniBehSerie',$DATA[2];
 	push @SQL, 'SerieZacina',$DATA[3];
-	my $QUERY2 = $dbh->prepare("select tblRuns.`Primary`, DATE_FORMAT((tblRuns.Date), '%d. %m. %Y') from tblRuns where Date(tblRuns.Date) =\"".$DATA[3]."\";");
+	my $QUERY2 = $dbh->prepare("select tblRuns.`Primary` from tblRuns where Date(tblRuns.Date) = \'$DATA[5]\';");
 	$QUERY2->execute() or die "Nepovedlo se nacist statistiky 8!";
 	while (my @DATA2 =  $QUERY2->fetchrow_array) {
 		push @SQL, 'PrvniBehSerie',$DATA2[0];		
@@ -97,8 +97,8 @@ while (@DATA =  $QUERY->fetchrow_array) {
 $QUERY = $dbh->prepare("select distinct YEAR(convert_tz(Date, 'UTC','CET')) AS Rok, ROUND(SUM(Distance/1000), 2) AS Distance, SUM(Duration) AS SecForPace from tblRuns where YEAR(Date) = 2010 AND StartAddress IS NOT NULL;");
 $QUERY->execute() or die "Nepovedlo se nacist statistiky 12!";
 while (@DATA =  $QUERY->fetchrow_array) {
-	push @SQL, $DATA[0].'.Distance.'.$DATA[0],$DATA[1];
-	push @SQL, $DATA[0].'.SecForPace.'.$DATA[0],$DATA[2];
+	push @SQL, $DATA[0].'.KorDistance',$DATA[1];
+	push @SQL, $DATA[0].'.KorSecForPace',$DATA[2];
 }
 
 $QUERY = $dbh->prepare(q{INSERT INTO tblStats (Nazev, Hodnota1) VALUES (?, ?)});
